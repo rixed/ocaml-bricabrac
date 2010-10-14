f2be690fa06b10b1bbdb694ad424b50e63eb37f9
Cedric Cellier <rixed@happyleptic.org>
2010-10-14 06:47:18 +0200

    First import

---
+++ Makefile
@@ -0,0 +1,42 @@
+OCAMLPATH = ..
+
+all: bricabrac.cma
+opt: bricabrac.cmxa
+
+NAME = bricabrac
+
+ML_SOURCES = bricabrac.ml
+
+REQUIRES =
+
+include make.common
+
+.PHONY: all opt install uninstall reinstall
+
+$(NAME).cma: $(ML_OBJS)
+	$(OCAMLC)   -a -o $@ -package "$(REQUIRES)" $(OCAMLFLAGS) $(ML_OBJS)
+
+$(NAME).cmxa: $(ML_XOBJS)
+	$(OCAMLOPT) -a -o $@ -package "$(REQUIRES)" $(OCAMLOPTFLAGS) $(ML_XOBJS)
+
+install: all
+	if test -f $(NAME).cmxa ; then extra="$(NAME).cmxa $(NAME).a" ; fi ; \
+	ocamlfind install $(NAME) *.cmi $(NAME).cma META $$extra
+
+uninstall:
+	ocamlfind remove $(NAME)
+
+reinstall: uninstall install
+
+check: $(NAME).cma $(NAME).cmxa
+	@make -C tests all opt
+	@for t in tests/*.byte tests/*.opt ; do $$t ; done
+	@echo Ok
+
+clean-spec:
+	@make -C tests clean
+
+distclean: clean
+	@rm -f glop_spec.ml make.conf
+
+include .depend
---
+++ bricabrac.ml
@@ -0,0 +1,70 @@
+let unopt = function
+	| Some x -> x
+	| None -> failwith "Cannot unopt None"
+
+let may o f = match o with
+	| Some x -> f x
+	| None -> ()
+
+let compose f g = fun x -> f (g x)
+
+let forever f x = while true do f x done
+
+let try_finalize f x finally y =
+  let res =
+    try f x with exn ->
+      finally y;
+      raise exn in
+    finally y;
+    res
+
+let with_mutex m f x =
+  Mutex.lock m ;
+  try_finalize f x Mutex.unlock m
+
+let with_fd fd f =
+  try_finalize f fd Unix.close fd
+
+let apply x f = f x
+
+let pi = 4. *. atan 1.
+
+let ignore_exceptions f x =
+  try ignore (f x) with _ -> ()
+
+let rec restart_on_EINTR f x =
+  try f x with Unix.Unix_error (Unix.EINTR, _, _) ->
+    restart_on_EINTR f x
+
+let string_split line delim =
+  let rec aux strs i =
+    if i >= String.length line then List.rev strs
+    else let next_i =
+      try String.index_from line i delim with Not_found -> String.length line in
+    let substr = String.sub line i (next_i - i) in
+      aux (substr :: strs) (next_i + 1) in
+    aux [] 0
+
+let rec join elt_to_string str list =
+  match list with
+    | [] -> ""
+    | [head] -> elt_to_string head
+    | head :: tail ->
+        (elt_to_string head)^str^(join elt_to_string str tail)
+
+let list_to_string elt_to_string list =
+  join elt_to_string "\n" list
+
+let list_intersect l1 l2 =
+  List.filter (fun e -> List.mem e l2) l1
+
+let list_sub l1 l2 =
+  List.filter (fun e -> not (List.mem e l2)) l1
+
+let list_init n f =
+  let rec aux prevs n =
+    if n = 0 then prevs else
+      aux (f () :: prevs) (n - 1) in
+    aux [] n
+
+
